7ef9b60147fdd02f0cef8a38a97f834de9042a2c
AmosMwangi <amosmwangi101@gmail.com>
2019-12-15 22:23:52 +0300

    configured media url

--- pics/urls.py
+++ pics/urls.py
@@ -11,3 +11,5 @@ urlpatterns = [
     path('search/', views.search_results, name = 'search_results'),
     path('pics/<int:pics_id>',views.pics,name ='pics'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
