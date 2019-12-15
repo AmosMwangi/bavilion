769e3462066a6a814b2b0d9b47cdaf9236430f0d
AmosMwangi <amosmwangi101@gmail.com>
2019-12-15 22:23:20 +0300

    created url pattern

--- pics/urls.py
+++ pics/urls.py
@@ -4,3 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+urlpatterns = [
+    path('', views.pics_of_day, name = 'picsToday'),
+    path('detail/<int:pkid>', views.pics_details, name = 'detail'),
+    path('archives/<str:past_date>', views.past_days_pics, name = 'pastPics'),
+    path('search/', views.search_results, name = 'search_results'),
+    path('pics/<int:pics_id>',views.pics,name ='pics'),
+]
