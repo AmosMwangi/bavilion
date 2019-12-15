f0f747dada3fd2c74246715df93568c310fd5b62
AmosMwangi <amosmwangi101@gmail.com>
2019-12-15 22:28:39 +0300

    changes some url names

--- pics/urls.py
+++ pics/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.pics_of_day, name = 'picsToday'),
-    path('detail/<int:pkid>', views.pics_details, name = 'detail'),
+    path('', views.home, name = 'home'),
+    path('detail/<int:pkid>', views.detail, name = 'detail'),
     path('archives/<str:past_date>', views.past_days_pics, name = 'pastPics'),
     path('search/', views.search_results, name = 'search_results'),
     path('pics/<int:pics_id>',views.pics,name ='pics'),
