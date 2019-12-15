750d65d76578dfbc9868399098dc2f3b2c469baf
AmosMwangi <amosmwangi101@gmail.com>
2019-12-15 22:46:06 +0300

    made pics class

--- pics/models.py
+++ pics/models.py
@@ -40,3 +40,33 @@ class tags(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Pics(models.Model):
+    title = models.CharField(max_length=60)
+    post = models.TextField(max_length=1000)
+    editor = models.ForeignKey(Editor, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    cover = models.ImageField(default="default.jpg", upload_to="images/")
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def todays_pics(cls):
+        today = dt.date.today()
+        bird = cls.objects.filter(pub_date__date=today)
+        return bird
+
+    @classmethod
+    def days_pics(cls, date):
+        bird = cls.objects.filter(pub_date__date=date)
+        return bird
+
+    @classmethod
+    def search_by_category(cls, search_term):
+        bird = cls.objects.filter(category__icontains=search_term)
+        return bird
