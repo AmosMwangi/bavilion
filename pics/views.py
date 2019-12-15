117a4ba86f72de0102309206e0a9afbe67321b08
AmosMwangi <amosmwangi101@gmail.com>
2019-12-15 22:31:59 +0300

    created home view function

--- pics/views.py
+++ pics/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render,redirect
 import datetime as dt
 from django.http import HttpResponse,Http404
-from .models import Details
+from .models import Pics
+
+# display home 
+def home(request):
+ 
+    pics = Pics.todays_pics()
+    return render(request, 'folder/home.html', {"pics":pics})
+    
