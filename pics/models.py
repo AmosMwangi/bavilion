c12ccc782d56dd238c16c10dd19cc9a9ef5261ad
AmosMwangi <amosmwangi101@gmail.com>
2019-12-15 22:42:36 +0300

    made editor class

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+
+class Editor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=10, blank=True)
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ["first_name"]
+
