e3742b642bb5f8f69a63507cdd21284307ecca7d
AmosMwangi <amosmwangi101@gmail.com>
2019-12-15 22:40:30 +0300

    mae view function to display past_days_pics

--- pics/views.py
+++ pics/views.py
@@ -18,3 +18,18 @@ def detail(request,pkid):
 
     return render(request, 'folder/details.html', {"image":image})
 
+# display past days pics
+def past_days_pics(request, past_date):
+    try:
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+    
+    except ValueError:
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(picsToday)
+
+    bird = Pics.days_pics(date)
+    return render(request, 'all-pics/past-pics.html', {"date":date,"bird":bird, "title":"Past News"})
+
