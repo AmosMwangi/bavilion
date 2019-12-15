b5c069f77ecab17e1eda29e9efc1fab90d368a0c
AmosMwangi <amosmwangi101@gmail.com>
2019-12-15 22:50:51 +0300

    made admin class

--- pics/admin.py
+++ pics/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 from .models import Editor,Pics,tags,Category,Location
 
 # Register your models here.
+class PicsAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+    admin.site.register(Editor)
+    admin.site.register(Pics,PicsAdmin)
+    admin.site.register(tags)
+    admin.site.register(Category)
+    admin.site.register(Location)
