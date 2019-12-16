54bba675980c778d75d7f2a43eb1953f11e39547
AmosMwangi <amosmwangi101@gmail.com>
2019-12-16 04:53:43 +0300

    registerd models in the admin

--- pics/admin.py
+++ pics/admin.py
@@ -4,8 +4,9 @@ from .models import Editor,Pics,tags,Category,Location
 # Register your models here.
 class PicsAdmin(admin.ModelAdmin):
     filter_horizontal =('tags',)
-    admin.site.register(Editor)
-    admin.site.register(Pics,PicsAdmin)
-    admin.site.register(tags)
-    admin.site.register(Category)
-    admin.site.register(Location)
+
+admin.site.register(Editor)
+admin.site.register(Pics,PicsAdmin)
+admin.site.register(tags)
+admin.site.register(Category)
+admin.site.register(Location)
